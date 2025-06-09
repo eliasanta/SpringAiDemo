fbbfc966a93a602631094b136376f1138d529c42
Elia <eliasanta93@gmail.com>
2025-06-09 10:49:17 +0200

    first commit

---
+++ SpringAiDemo/src/main/java/com/ai/SpringAiDemo/GenAiController.java
@@ -0,0 +1,52 @@
+package com.ai.SpringAiDemo;
+
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class GenAiController {
+    private final ChatService chatService;
+    private final ImageService imageService;
+    private final RecipeService recipeService;
+
+
+    public GenAiController(ChatService chatService, ImageService imageService, RecipeService recipeService) {
+        this.chatService = chatService;
+        this.imageService =imageService;
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/ask-ai")
+    public String getResponse(@RequestParam String prompt){
+        return chatService.getResponse(prompt);
+    }
+
+    @GetMapping("/ask-ai-options")
+    public String getResponseOptions(@RequestParam String prompt){
+        return chatService.getResponseOptions(prompt);
+    }
+
+//non funziona perchè credo che il modello che sto utilizzando non lo supporti
+    @GetMapping("/generate-img")
+    public void generateImages (HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse= imageService.generateImage(prompt);
+        String imageUrl= imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(imageUrl);
+    }
+
+    @GetMapping("/recipe-creator")
+    public String recipeCreator(@RequestParam String ingredients,
+                                @RequestParam(defaultValue = "italiana") String cuisine,
+                                @RequestParam(defaultValue = "nessuna") String dietaryRestrictions) {
+        return recipeService.createRecipe(ingredients, cuisine, dietaryRestrictions);
+    }
+
+
+}
